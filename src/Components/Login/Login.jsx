2b6958f0afd021998b61809f25ce04546f1a7464
Gabriel Alves <gabrielalvesbosca@gmail.com>
2025-01-08 15:35:51 -0300

    Initial commit

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,23 @@
+import { FaUser, FaLock } from "react-icons/fa";
+
+const Login = () => {
+  return (
+      <div className="container">
+        <form>
+            <h1>Acesse o sistema</h1>
+            <div>
+                <input type="email" placeholder="E-mail" />
+                <FaUser className="icon"/>
+            </div>
+            <div>
+                <input type="password" placeholder="Password" />
+                <FaLock  className="icon"/>
+            </div>
+            <button>Entrar</button>
+        </form>
+      </div>
+  )
+}
+
+export default Login
+
