c3ce5b9ac573a1329ebfdec99b71ec1697d8d51b
Gabriel Alves <gabrielalvesbosca@gmail.com>
2025-01-08 15:43:59 -0300

    Update Login.jsx

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,22 +1,45 @@
 import { FaUser, FaLock } from "react-icons/fa";
+import { useState } from "react";
+
+import "./Login.css";
 
 const Login = () => {
-  return (
-      <div className="container">
-        <form>
-            <h1>Acesse o sistema</h1>
-            <div>
-                <input type="email" placeholder="E-mail" />
-                <FaUser className="icon"/>
-            </div>
-            <div>
-                <input type="password" placeholder="Password" />
-                <FaLock  className="icon"/>
-            </div>
-            <button>Entrar</button>
-        </form>
-      </div>
-  )
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    
+    const handleSubmit = () =>{
+        console.log("Envio");
+    };
+
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <h1>Acesse o sistema</h1>
+                <div>
+                    <input type="email" placeholder="E-mail" />
+                    <FaUser className="icon"/>
+                </div>
+                <div>
+                    <input type="password" placeholder="Password" />
+                    <FaLock  className="icon"/>
+                </div>
+                <div className="recall-forget">
+                    <label>
+                        <input type="checkbox"/>
+                        Lembre de mim
+                    </label>
+                    <a href="#">Esqueceu a senha?</a>
+                </div>
+                <button>Entrar</button>
+                <div className="signup-link">
+                    <p>
+                        Não tem uma conta? <a href="#">Registrar</a>
+                    </p>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Login
